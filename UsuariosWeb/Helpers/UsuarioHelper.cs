8d26f4766306c541885b4329d79c843298adf5ad
marcus-vinicius-nunes <vinicius_rj87@yahoo.com.br>
2024-04-14 09:55:39 -0300

    first commit

---
+++ UsuariosWeb/Helpers/UsuarioHelper.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+using UsuariosWeb.Dtos;
+
+namespace UsuariosWeb.Helpers
+{
+    /// <summary>
+    /// Classe para execucao de operações do sistema com o usuário
+    /// </summary>
+    public class UsuarioHelper
+    {
+        private readonly ILocalStorageService _storageService;
+
+        public UsuarioHelper(ILocalStorageService storageService)
+        {
+            _storageService = storageService;
+        }
+
+        public async Task SignIn(UsuarioDto dto)
+        {
+            await _storageService.SetItemAsync("user-auth", dto);
+        }
+    }
+}
